b9279ed81061f4a9aa339066a0309502064b5af0
FedeP <f.pulcino@campus.unimib.it>
2024-08-30 08:18:33 +0200

    fix part2 output

--- part2/main.py
+++ part2/main.py
@@ -4,6 +4,8 @@ from compression.dct_functions import compress_image
 from compression.padding import add_padding, remove_padding
 from cli.image_selection import select_image
 from cli.user_input import get_window_size, get_threshold
+import matplotlib.pyplot as plt
+
 
 # Funzione principale per il compressore di immagini con CLI
 def main():
@@ -39,12 +41,27 @@ def main():
     compressed_img = remove_padding(compressed_img, original_M, original_N)
 
     # Salva l'immagine compressa nella cartella 
-    compress_image_name = f"compressed_{selected_image}"
+    compress_image_name = f"compressed_F{F}_d{d}_{selected_image}"
     compressed_image_path = os.path.join(os.getcwd(), "out", compress_image_name)
-    
+
+    # Salva l'immagine compressa
     save_image(compressed_img, compressed_image_path)
 
-    print(f"Compressed image saved at: {compressed_image_path}")
+    # Carica l'immagine compressa per la visualizzazione
+    compressed_img_loaded = load_image(compressed_image_path)
+
+    # Mostra l'immagine originale e quella compressa affiancate
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+    axes[0].imshow(img, cmap='gray')
+    axes[0].set_title('Original Image')
+    axes[0].axis('off')
+
+    axes[1].imshow(compressed_img_loaded, cmap='gray')
+    axes[1].set_title('Compressed Image, F = {}, d = {:.2f}'.format(F, d))
+    axes[1].axis('off')
+
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
